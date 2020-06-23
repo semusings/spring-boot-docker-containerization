7ee8eecd77d6fb539c31da47ba31c51825a746f6
Bhuwan Upadhyay <bhuwan.upadhyay@clusus.com>
2020-06-23 20:08:03 +0545

    docs: spring boot docker containerization

---
+++ src/main/kotlin/io/github/bhuwanupadhyay/example/SpringBoot.kt
@@ -0,0 +1,11 @@
+package io.github.bhuwanupadhyay.example
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBoot
+
+fun main(args: Array<String>) {
+	runApplication<SpringBoot>(*args)
+}
---
+++ src/test/kotlin/io/github/bhuwanupadhyay/example/SpringBootTests.kt
@@ -0,0 +1,13 @@
+package io.github.bhuwanupadhyay.example
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
